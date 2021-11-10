32e716d179bf1d64c64fdbff63623090596ec24b
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 15:01:56 -0300

    Criar classe Controller

---
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaService.java
@@ -0,0 +1,10 @@
+package br.zup.GerenciadorZup.gerenciador;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    private ContaRepository contaRepository;
+
+}
