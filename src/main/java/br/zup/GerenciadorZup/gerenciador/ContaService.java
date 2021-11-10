65e044f8e4d94634282f45173e175dd52ee95a81
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 17:54:25 -0300

    Atualizar metodo salvar conta

--- src/main/java/br/zup/GerenciadorZup/gerenciador/ContaService.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaService.java
@@ -1,13 +1,25 @@
 package br.zup.GerenciadorZup.gerenciador;
 
+import br.zup.GerenciadorZup.gerenciador.enuns.Status;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
 
     private ContaRepository contaRepository;
 
-    public Conta salvarConta (Conta conta) {
+    public Conta salvarConta(Conta conta) {
+
+        conta.setDataDePagamento(null);
+        LocalDate dataAtual = LocalDate.now();
+
+        if (conta.getDataDeVencimento().isBefore(dataAtual)) {
+            conta.setStatus(Status.VENCIDA);
+        } else {
+            conta.setStatus(Status.AGUARDANDO);
+        }
 
         return contaRepository.save(conta);
 
