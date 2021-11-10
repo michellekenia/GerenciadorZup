ce18d8ae9ae36a0b334f654e21fd27fa1789b8dd
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 19:21:53 -0300

    Arrumar sintaxe

--- src/main/java/br/zup/GerenciadorZup/gerenciador/ContaRepository.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaRepository.java
@@ -2,7 +2,11 @@ package br.zup.GerenciadorZup.gerenciador;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContaRepository extends CrudRepository <Conta, Integer > {
+import javax.print.attribute.IntegerSyntax;
 
+public interface ContaRepository extends CrudRepository <Conta, Integer> {
 
 }
+
+
+
