d7ade2f347ebe0a880fa66c2ce44d78c27676ce3
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 14:05:00 -0300

    Primeiro commit

---
+++ src/main/java/br/zup/GerenciadorZup/GerenciadorZupApplication.java
@@ -0,0 +1,13 @@
+package br.zup.GerenciadorZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/zup/GerenciadorZup/GerenciadorZupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.zup.GerenciadorZup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
