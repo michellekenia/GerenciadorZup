8a4f9dbb5ea5d914fd9b3d23fd53b0721d034118
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 19:20:44 -0300

    Adicionar metodo de cadastro no controller

--- src/main/java/br/zup/GerenciadorZup/gerenciador/ContaController.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaController.java
@@ -1,19 +1,35 @@
 package br.zup.GerenciadorZup.gerenciador;
 
 
+import br.zup.GerenciadorZup.gerenciador.dtos.ContaDTO;
+import br.zup.GerenciadorZup.gerenciador.dtos.RetornoDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping ("/contas")
+@RequestMapping("/contas")
 public class ContaController {
 
     @Autowired
     private ContaService contaService;
-
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public RetornoDTO realizarCadastro (@RequestBody ContaDTO contaDTo) {
+
+    Conta conta = modelMapper.map(contaDTo , Conta.class);
+
+    RetornoDTO retornoDTO = modelMapper.map(contaService.salvarConta(conta) , RetornoDTO.class);
+
+
+     return retornoDTO;
+
+    }
+
 
 }
