439a80793861c0980b5e3315c8fe7cd2fb12298e
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-10 14:44:16 -0300

    Criar model Conta

---
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/Conta.java
@@ -0,0 +1,74 @@
+package br.zup.GerenciadorZup.gerenciador;
+
+import java.time.LocalDate;
+
+public class Conta {
+
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+    private LocalDate dataDePagamento;
+    private Status status;
+
+    public Conta() {
+    }
+
+    public Conta(String nome, double valor, Tipo tipo, LocalDate dataDeVencimento, LocalDate dataDePagamento, Status status) {
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.dataDeVencimento = dataDeVencimento;
+        this.dataDePagamento = dataDePagamento;
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDate getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDate dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+}
