94d24e4dc6a70e22801c27f6ec1020c73c89f5ec
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 12:51:11 -0300

    Incluir data de pagamento no metodo atualizarConta

--- src/main/java/br/zup/GerenciadorZup/gerenciador/ContaService.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +51,7 @@ public class ContaService {
 
         Conta conta = buscarContaId(id);
         conta.setStatus(Status.PAGO);
+        conta.setDataDePagamento(LocalDateTime.now());
         contaRepository.save(conta);
 
         return conta;
