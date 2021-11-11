8b2dca495d349467b8821e3fd9351b734208e2a1
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 12:52:10 -0300

    Refatorar DTO para receber somente o status

--- src/main/java/br/zup/GerenciadorZup/gerenciador/dtos/StatusDTO.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/dtos/StatusDTO.java
@@ -6,65 +6,12 @@ import br.zup.GerenciadorZup.gerenciador.enuns.Tipo;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class ContasPagasDTO {
+public class StatusDTO {
 
-    private int id;
-    private String nome;
-    private double valor;
-    private Tipo tipo;
-    private LocalDate dataDeVencimento;
-    private LocalDateTime dataDePagamento;
+
     private Status status;
 
-    public ContasPagasDTO() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public Tipo getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(Tipo tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDate getDataDeVencimento() {
-        return dataDeVencimento;
-    }
-
-    public void setDataDeVencimento(LocalDate dataDeVencimento) {
-        this.dataDeVencimento = dataDeVencimento;
-    }
-
-    public LocalDateTime getDataDePagamento() {
-        return dataDePagamento;
-    }
-
-    public void setDataDePagamento(LocalDateTime dataDePagamento) {
-        this.dataDePagamento = dataDePagamento;
+    public StatusDTO() {
     }
 
     public Status getStatus() {
