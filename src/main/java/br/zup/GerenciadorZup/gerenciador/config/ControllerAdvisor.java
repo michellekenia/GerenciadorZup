5079057f7ea3fc552a2a1a48335ece7a02116d07
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 19:35:01 -0300

    Criar metodo para excecao de status invalido

--- src/main/java/br/zup/GerenciadorZup/gerenciador/config/ControllerAdvisor.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/config/ControllerAdvisor.java
@@ -1,6 +1,7 @@
 package br.zup.GerenciadorZup.gerenciador.config;
 
 import br.zup.GerenciadorZup.gerenciador.exceptions.IdNaoEncontrado;
+import br.zup.GerenciadorZup.gerenciador.exceptions.StatusInvalido;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,7 +31,14 @@ public class ControllerAdvisor {
 
     @ExceptionHandler (IdNaoEncontrado.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public MensagemDeErro manipularExcecaoNomeInvalido(IdNaoEncontrado exception) {
+    public MensagemDeErro manipularExcecaoIdNaoEncontrado(IdNaoEncontrado exception) {
+
+        return new MensagemDeErro(exception.getMessage());
+    }
+
+    @ExceptionHandler (StatusInvalido.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro manipularExcecaoStatusInvalido(StatusInvalido exception) {
 
         return new MensagemDeErro(exception.getMessage());
     }
