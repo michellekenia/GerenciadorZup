b248a16c5fa0a7431f914889faf1fb74ca8b90b9
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 19:32:39 -0300

    Criar classe para excecao de status da conta

---
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/exceptions/StatusInvalido.java
@@ -0,0 +1,10 @@
+package br.zup.GerenciadorZup.gerenciador.exceptions;
+
+public class StatusInvalido extends RuntimeException {
+
+    public StatusInvalido (String message) {
+
+        super(message);
+    }
+
+}
