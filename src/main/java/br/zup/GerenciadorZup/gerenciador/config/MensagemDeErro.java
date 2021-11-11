88270e7efdce3d38e27fd2d6a1b7270b1400136f
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 15:16:51 -0300

    Criar classe mensagem de erro

---
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/config/MensagemDeErro.java
@@ -0,0 +1,18 @@
+package br.zup.GerenciadorZup.gerenciador.config;
+
+public class MensagemDeErro {
+
+    private String mensagemDeErro;
+
+    public MensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+
+    public String getMensagemDeErro() {
+        return mensagemDeErro;
+    }
+
+    public void setMensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+}
