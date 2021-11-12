ff808e01e08f9fb0986f9b3dd51a2cce7c7f122b
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-11 22:24:58 -0300

    Criar listas para filtro

--- src/main/java/br/zup/GerenciadorZup/gerenciador/ContaRepository.java
+++ src/main/java/br/zup/GerenciadorZup/gerenciador/ContaRepository.java
@@ -1,10 +1,19 @@
 package br.zup.GerenciadorZup.gerenciador;
 
+import br.zup.GerenciadorZup.gerenciador.enuns.Status;
+import br.zup.GerenciadorZup.gerenciador.enuns.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.print.attribute.IntegerSyntax;
+import java.util.List;
 
 public interface ContaRepository extends CrudRepository <Conta, Integer> {
+
+    List <Conta> findAllByStatus (Status status);
+
+    List <Conta> findAllByTipo (Tipo tipo);
+
+    List <Conta> findAllByValor (double valor);
 
 }
 
